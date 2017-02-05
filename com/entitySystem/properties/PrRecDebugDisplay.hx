9601d0835326662e00e7e3fbe828c38a0d7b3473
Joaquin Bello <juakob@gmail.com>
2017-02-05 04:16:18 -0300

    initial debug rec working

--- com/entitySystem/properties/PrRecDebugDisplay.hx
+++ com/entitySystem/properties/PrRecDebugDisplay.hx
@@ -16,19 +16,21 @@ class PrRecDebugDisplay implements Property
 	@ignore
 	public var varsSource:Array<Int>;
 
-	public var x:Float;
-	public var y:Float;
-	public var width:Float;
-	public var height:Float;
-	public var offsetX:Float;
-	public var offsetY:Float;
-	public var scaleX:Float;
-	public var scaleY:Float;
+	public var x:Float=0;
+	public var y:Float=0;
+	public var width:Float=10;
+	public var height:Float=10;
+	public var offsetX:Float=0;
+	public var offsetY:Float=0;
+	public var scaleX:Float=1;
+	public var scaleY:Float=1;
 	
 	
 	public function new() 
 	{
-		
+		propertysSource = new Array();
+		varsDestination = new Array();
+		varsSource = new Array();
 	}
 	
 	public function serialize():String 
